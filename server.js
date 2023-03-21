2d84d8fe855b36f020a81c74a1f3fc0fca8c7d6f
herilion <heritierlionge@yahoo.com>
2023-03-21 13:21:11 +0200

    run server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port=3000;
+
+app.get('/', (req,res) => {
+
+	res.sendFile(__dirname + '/NiceAdmin/index.html')
+})
+app.use('/',express.static(__dirname + '/NiceAdmin'))
+app.listen(3000, () => {
+	console.log("server is running on port" + port)
+})
